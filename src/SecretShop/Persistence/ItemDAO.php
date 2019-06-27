84fbf7534362e19588a3254d7e4e6f4b9557ba2b
YuriHermont1995 <49692400+YuriHermont1995@users.noreply.github.com>
2019-06-27 20:19:06 -0300

    Add files via upload

--- src/SecretShop/Persistence/ItemDAO.php
+++ src/SecretShop/Persistence/ItemDAO.php
@@ -4,7 +4,8 @@ include_once("../Model/ClassItem.php");
 	
 		function __construct(){
 		}
-//Função de cadastro de cliente no sistema
+//Função de cadastro de Item no sistema
+/*
 		function cadastrar($Item,$link){
 			
 			$insert ="INSERT INTO item(nome,descricao,preco,forca,agilidade,inteligencia,vida,mana,dano,armadura,velocidadeDeAtaque,habilidadeAtiva,descricaoAtiva,habilidadePassiva,descricaoPassiva)";
@@ -18,8 +19,9 @@ include_once("../Model/ClassItem.php");
 			}
 			echo "salvo com sucesso";
 		}
-
-//Função de Exclusao de cliente no sistema
+*/
+/*
+//Função de Exclusao de Item no sistema
 		function excluir($Item,$link){
 			$query = "DELETE FROM `Item` WHERE nome = '".$Item->getNome()."';";
 			if(!mysqli_query($link,$query)){
@@ -27,9 +29,9 @@ include_once("../Model/ClassItem.php");
 			}
 			echo "excluido com sucesso";
 		}
+*/
 
-
-//Função de select de um cliente no sistema
+//Função de select de um Item no sistema
 		function consultar($Item,$link){
 			$query = "SELECT * FROM `item` WHERE nome = '".$Item->getNome()."';";
 			$r = mysqli_query($link,$query);
@@ -41,8 +43,8 @@ include_once("../Model/ClassItem.php");
 			
 			return mysqli_fetch_assoc($r);
 		}
-
-//Função de Atualizaçao de cliente no sistema
+/*
+//Função de Atualizaçao de Item no sistema
 		function alterar($Item,$link){
 			echo $Item->getNome();
 			$query = "UPDATE item SET Nome='".$Item->getNome()."',atributo='".$Cliente->getAtributo()."',vida=".$Cliente->getVida(). ",mana=".$Cliente->getMana(). ",dano=".$Cliente->getDano(). ",forca=".$Cliente->getForca(). ",agilidade=".$Cliente->getAgilidade(). ",inteligencia=".$Cliente->getInteligencia(). ",armadura=".$Cliente->getArmadura(). ",velocidadeDeAtaque=".$Cliente->getVelocidadeAtq(). " WHERE CPF = '".$Cliente->getCpf()."';";
@@ -51,20 +53,6 @@ include_once("../Model/ClassItem.php");
 				die ("nao foi possivel alterar".mysqli_error($link));
 			}
 			echo "alterado com sucesso";
-		}
-
-/*
-//Função de selecionar todos os cliente do sistema
-		function consultarTodos($link){
-			$query = "SELECT * FROM `cliente`";
-			$r = mysqli_query($link, $query);
-
-			if (!$r) {
-				echo "Erro do banco de dados, não foi possível consultar o banco de dados\n";
-				echo 'Erro MySQL: ' . mysqli_error();
-				exit;
-			}
-			return $r;
 		}
 */
 
